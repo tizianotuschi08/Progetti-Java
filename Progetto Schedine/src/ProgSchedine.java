19082be54e8f266e018cf7bcc6568d95564f702b
tizianotuschi08 <tiziano.tuschi@gmail.com>
2025-10-27 20:27:16 +0100

    Add files via upload

---
+++ Progetto Schedine/src/ProgSchedine.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProgSchedine {
+	
+	static Scanner input = new Scanner(System.in);
+	
+	static Squadra Roma = new Squadra("Roma",false,28,1800,0,0,0,0,0,0,0);
+	static Squadra Lazio = new Squadra("Lazio",false,28,1800,0,0,0,0,0,0,0);
+	static Squadra Napoli = new Squadra("Napoli",false,28,1800,0,0,0,0,0,0,0);
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
